adf6deda0861ffcb895b4047b083fafae7f2b818
yasser471272 <120171428+yasser471272@users.noreply.github.com>
2022-12-13 12:49:44 +0200

    Add files via upload

---
+++ 1st project on python.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IM A KING OF WORLD 
+
+# **THE BEST WORIER**
+
+# *I WAS BORN ON IT *
+# <br>.
+# <br>where ?
+
+# |kitos|kitos|
+# |----|----|
+# |abdo|abdo|
+# |try|try2|
+
+# In[17]:
+
+
+bool(1)
+
+
+# In[27]:
+
+
+type(True)
+
+
+# In[31]:
+
+
+bool()
+
+
+# In[32]:
+
+
+60//2
+
+
+# In[33]:
+
+
+45//6
+
+
+# In[34]:
+
+
+45/3
+
+
+# In[35]:
+
+
+25/6
+
+
+# In[36]:
+
+
+25//6
+
+
+# In[43]:
+
+
+total_min=146
+total_min/60
+
+
+# In[93]:
+
+
+total_min=85
+total_hour=total_min/60
+
+
+# In[94]:
+
+
+total_hr=
+
+
+# In[96]:
+
+
+print("hello python")
+
+
+# In[102]:
+
+
+# Check the Python Version
+import sys
+print(sys.version)
+
+
+# In[104]:
+
+
+#check the python version 
+import sys
+print(sys.version)
+
+
+# In[128]:
+
+
+# Practice on writing comments
+
+print('=Hello, Python!') # This line prints a string
+print(' -Hi')
+print("-are u okey")
+print("-probably yes")
+
+
+# In[129]:
+
+
+print("Hello, world!") # Print the traditional hello world
+
+
+# In[133]:
+
+
+type("me")
+
+
+# In[134]:
+
+
+sys.float_info
+
+
+# In[135]:
+
+
+True(20)
+
+
+# In[136]:
+
+
+type(-1)
+
+
+# In[ ]:
+
+
+
+
